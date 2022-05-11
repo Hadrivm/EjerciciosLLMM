3ab2fb548283876cd3c03dc3bc86b065fef0b816
Hadrivm <91189372+Hadrivm@users.noreply.github.com>
2022-05-11 23:24:47 +0200

    Update js10.js

--- JavaScript/js10.js
+++ JavaScript/js10.js
@@ -30,3 +30,75 @@ function mostrarMayor1() {
     var num3 = document.getElementById('num3').value;
     document.getElementById("resultado1").innerHTML = numMayor1(nume1, nume2, num3);
 }
+
+// Ejercicio B
+maxOfThree(5, 4, 6);
+function maxOfThree(a, b, c) {
+  if (a > b) {
+    document.getElementById("resultado2").innerHTML = `El resultado es: ${a}`;
+    if (a < c) {
+      document.getElementById("resultado2").innerHTML = `El resultado es: ${c}`;
+    } else {
+      document.getElementById("resultado2").innerHTML = `El resultado es: ${a}`;
+    }
+  } else if (b > a) {
+    document.getElementById("resultado2").innerHTML = `El resultado es: ${b}`;
+    if (b < c) {
+      document.getElementById("resultado2").innerHTML = `El resultado es: ${c}`;
+    } else {
+      document.getElementById("resultado2").innerHTML = `El resultado es: ${b}`;
+    }
+  }
+}
+
+// EJercicio C
+esVocal("b");
+function esVocal(a) {
+  if (
+    a == "a" ||
+    a == "e" ||
+    a == "i" ||
+    a == "o" ||
+    a == "u" ||
+    a == "A" ||
+    a == "E" ||
+    a == "I" ||
+    a == "O" ||
+    a == "U"
+  ) {
+    document.getElementById(
+      "resultado3"
+    ).innerHTML = `El valor ${a} es una vocal`;
+  } else {
+    document.getElementById(
+      "resultado3"
+    ).innerHTML = `El valor ${a} no es una vocal`;
+  }
+}
+
+
+// Ejercicio E
+sum();
+mul();
+function sum() {
+  const lista = [1, 2, 3, 4];
+  let res = lista.reduce((a, b) => {
+    return a + b;
+  });
+  document.getElementById('resultado5suma').innerHTML = `Resultado de la suma: ${res}`
+}
+
+function mul() {
+  const lista = [1, 2, 3, 4];
+  let res = lista.reduce((a, b) => {
+    return a * b;
+  });
+  document.getElementById('resultado5multi').innerHTML = `Resultado de la multiplicación: ${res}`
+}
+
+
+// Ejercicio F
+document.getElementById('resultadof').innerHTML = reversa('Texto al revés')
+function reversa(s) {
+  return s.split("").reverse().join('')
+}
